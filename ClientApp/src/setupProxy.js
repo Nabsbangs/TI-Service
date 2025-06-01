2d6dcf0a9262bb639f3fff1746ea6f9c82f9d1a1
Nabsbangs <59548218+Nabsbangs@users.noreply.github.com>
2025-06-01 13:35:17 -0700

    Add files via upload

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -2,17 +2,17 @@ const createProxyMiddleware = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:29609';
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:29609';
 
-const context =  [
-  "/weatherforecast",
+const context = [
+    "/weatherforecast",
 ];
 
-module.exports = function(app) {
-  const appProxy = createProxyMiddleware(context, {
-    target: target,
-    secure: false
-  });
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: target,
+        secure: false
+    });
 
-  app.use(appProxy);
+    app.use(appProxy);
 };
